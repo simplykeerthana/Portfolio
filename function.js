a1511ea75a80aee66d2d334be7cd5de7526f68d3
Keerthana Madhavan <34112414+Madhakee01@users.noreply.github.com>
2022-04-12 21:57:42 -0400

    initial start:

---
+++ function.js
@@ -0,0 +1,11 @@
+// Code By Webdevtrick ( https://webdevtrick.com ) 
+
+$(document).ready(function(){ 
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('.navbar-default a').removeClass('selected');
+		$(this).addClass('selected');
+  });
+
+});
